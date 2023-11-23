6f925e34b028f379bd8c4e7bf6db48fced85c107
thevickypedia <vignesh.sivanandarao@gmail.com>
2023-11-22 18:35:10 -0600

    Remove default value for custom level Add optional logger as an argument Set OS based requirements Bump version

--- pybrightness/controller.py
+++ pybrightness/controller.py
@@ -1,11 +1,11 @@
 """Parent module to control brightness."""
 
-import os
+import logging
 import subprocess
+from typing import Union, List
 
-from .module import settings
+from .module import settings, commands
 
-POWERSHELL = '(Get-WmiObject -Namespace root/WMI -Class WmiMonitorBrightnessMethods).WmiSetBrightness(1,' + '{l}' + ')'
 OS_ERROR = OSError("Package is unsupported in %s" % settings.operating_system)
 
 
@@ -22,35 +22,48 @@ def eval_linux() -> None:
         )
 
 
-def increase() -> None:
+def _run(cmd: Union[str, List[str]]) -> None:
+    """Runs the command using subprocess module.
+
+    Args:
+        cmd: Command to run.
+    """
+    result = subprocess.run(cmd, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    if result.returncode and settings.logger:
+        settings.logger.error("Command `%s` failed with error code: %d", result.args, result.returncode)
+
+
+def increase(logger: logging.Logger = None) -> None:
     """Increases the brightness to maximum."""
+    settings.logger = logger
     if settings.operating_system == "Darwin":
         for _ in range(32):
-            os.system("""osascript -e 'tell application "System Events"' -e 'key code 144' -e ' end tell'""")
+            _run(commands.MAC_INCREASE)
     elif settings.operating_system == "Windows":
-        subprocess.run(["powershell", POWERSHELL.format(l=100)])
+        _run(["powershell", commands.WINDOWS.format(l=100)])
     elif settings.operating_system == "Linux":
         eval_linux()
-        os.system("echo %s | sudo -S brightnessctl s 100 > /dev/null" % settings.root_password)
+        _run(f"echo {settings.root_password} | sudo -S brightnessctl s 100 > /dev/null")
     else:
         raise OS_ERROR
 
 
-def decrease() -> None:
+def decrease(logger: logging.Logger = None) -> None:
     """Decreases the brightness to minimum."""
+    settings.logger = logger
     if settings.operating_system == "Darwin":
         for _ in range(32):
-            os.system("""osascript -e 'tell application "System Events"' -e 'key code 145' -e ' end tell'""")
+            _run(commands.MAC_DECREASE)
     elif settings.operating_system == "Windows":
-        subprocess.run(["powershell", POWERSHELL.format(l=0)])
+        _run(["powershell", commands.WINDOWS.format(l=0)])
     elif settings.operating_system == "Linux":
         eval_linux()
-        os.system("echo %s | sudo -S brightnessctl s 0 > /dev/null" % settings.root_password)
+        _run(f"echo {settings.root_password} | sudo -S brightnessctl s 0 > /dev/null")
     else:
         raise OS_ERROR
 
 
-def custom(percent: int = 50) -> None:
+def custom(percent: int, logger: logging.Logger = None) -> None:
     """Set brightness to a custom level.
 
     - | Since package uses in-built apple script (for macOS), the only way to achieve this is to set the
@@ -58,15 +71,18 @@ def custom(percent: int = 50) -> None:
 
     Args:
         percent: Percentage of brightness to be set.
+        logger: Bring your own logger.
     """
+    settings.logger = logger
+    assert isinstance(percent, int) and 0 <= percent <= 100, "value should be an integer between 0 and 100"
     if settings.operating_system == "Darwin":
         decrease()
         for _ in range(round((32 * int(percent)) / 100)):
-            os.system("""osascript -e 'tell application "System Events"' -e 'key code 144' -e ' end tell'""")
+            _run(commands.MAC_INCREASE)
     elif settings.operating_system == "Windows":
-        subprocess.run(["powershell", POWERSHELL.format(l=percent)])
+        _run(["powershell", commands.WINDOWS.format(l=percent)])
     elif settings.operating_system == "Linux":
         eval_linux()
-        os.system("echo %s | sudo -S brightnessctl s %d > /dev/null" % (settings.root_password, percent))
+        _run(f"echo {settings.root_password} | sudo -S brightnessctl s {percent} > /dev/null")
     else:
         raise OS_ERROR
--- pybrightness/module.py
+++ pybrightness/module.py
@@ -1,12 +1,17 @@
 """Parent module for settings wrapper."""
 
-import os
+import logging
 import platform
-from typing import AnyStr
+from typing import AnyStr, Union
 
-import dotenv
 
-dotenv.load_dotenv(dotenv_path=".env")
+def _get_root_pass() -> Union[str, None]:
+    """Get root password from environment variables for Linux machines."""
+    import os
+    import dotenv
+    dotenv.load_dotenv(dotenv_path=os.environ.get("env_file", os.environ.get("ENV_FILE", ".env")))
+    rp = os.environ.get('ROOT_PASSWORD') or os.environ.get('root_password')
+    return rp
 
 
 class Settings:
@@ -17,7 +22,28 @@ class Settings:
     """
 
     operating_system: str = platform.system()
-    root_password: AnyStr = os.environ.get('ROOT_PASSWORD') or os.environ.get('root_password')
+    if operating_system not in ("Linux", "Darwin", "Windows"):
+        raise OSError(
+            "Package is unsupported in %s" % operating_system
+        )
+    if operating_system == "Linux":
+        root_password: AnyStr = _get_root_pass()
+    logger: logging.Logger = None
 
 
 settings = Settings()
+
+
+class Commands:
+    """Unique commands for macOS and Windows.
+
+    >>> Commands
+
+    """
+
+    MAC_DECREASE = """osascript -e 'tell application "System Events"' -e 'key code 145' -e ' end tell'"""
+    MAC_INCREASE = """osascript -e 'tell application "System Events"' -e 'key code 144' -e ' end tell'"""
+    WINDOWS = '(Get-WmiObject -Namespace root/WMI -Class WmiMonitorBrightnessMethods).WmiSetBrightness(1,' + '{l}' + ')'
+
+
+commands = Commands()
